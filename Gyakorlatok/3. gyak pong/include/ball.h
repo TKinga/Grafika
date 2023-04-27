9f3d43a215a9dc5bf443b79f4703e2e67b705e8b
KinderKingu <90756594+TKinga@users.noreply.github.com>
2023-04-27 11:13:41 +0200

    Add files via upload

---
+++ Gyakorlatok/3. gyak pong/include/ball.h
@@ -0,0 +1,39 @@
+#ifndef BALL_H
+#define BALL_H
+
+/**
+ * Ball position and speed
+ */
+typedef struct Ball
+{
+    float x;
+    float y;
+    float radius;
+    float speed_x;
+    float speed_y;
+} Ball;
+
+/**
+ * Move the ball to the given position.
+ */
+void init_ball(Ball* ball, float x, float y);
+
+/**
+ * Update the ball.
+ * @param time elapsed time in seconds.
+ */
+void update_ball(Ball* ball, double time);
+
+/**
+ * Render the ball.
+ */
+void render_ball(Ball* ball);
+
+void set_ball_position(Ball*ball, float x, float y);
+
+void inc_ball_size(Ball*ball, float radius);
+
+void dec_ball_size(Ball*ball, float radius);
+
+
+#endif /* BALL_H */
